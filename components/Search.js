310e903fff38f609a9a47411386e4250778bd41e
Anna Kowalczuk <anna@kowalczuk.eu>
2018-09-16 18:11:03 +0200

    Fix search submit

--- components/Search.js
+++ components/Search.js
@@ -1,35 +1,33 @@
 Search = React.createClass({
     getInitialState() {
-        return {searchingText: ''};
+        return {
+          searchingText: ''
+        };
     },
-
-    handleChange: function (event) {
+    
+    handleChange: function(event) {
         var searchingText = event.target.value;
-        this.setState({searchingText: searchingText});
+        this.setState({
+            searchingText: searchingText
+        });
 
-        if (searchingText.length > 2) {
-            this
-                .props
-                .onSearch(searchingText);
-        }
+        // if (searchingText.length > 2) {
+        //     this.props.onSearch(searchingText);
+        //   }
     },
 
-    handleSubmit: function (event) {
-        this
-            .props
-            .onSearch(this.state.searchingText);
+    handleSubmit: function(event) {
+        this.props.onSearch(searchingText);
     },
 
-    handleKeyUp: function (event) {
+    handleKeyUp: function(event) {
         //Czy wciśnięty klawisz to enter?
-        if (event.keyCode === 13) {
-            this
-                .props
-                .onSearch(this.state.searchingText);
+        if (event.keyCode === 13) { 
+          this.props.onSearch(this.state.searchingText);
         }
     },
 
-    render: function () {
+    render: function() {
         var styles = {
             fontSize: '1.5em',
             width: '90%',
@@ -37,16 +35,17 @@ Search = React.createClass({
         };
 
         return (
-            <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                 <input
                     type="text"
                     onChange={this.handleChange}
                     onKeyUp={this.handleKeyUp}
                     placeholder="Tutaj wpisz wyszukiwaną frazę"
                     style={styles}
-                    value={this.state.searchTerm}/>
-                <input type="submit" value="Submit"/>
-            </form>
+                    value={this.state.searchTerm}
+                />
+                <input type="submit" value="Submit" />
+                </form>
         );
     }
 });
